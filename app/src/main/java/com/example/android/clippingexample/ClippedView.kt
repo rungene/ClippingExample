2b028938773a0c16e5e71a3e911f7ce91a1080fa
rungene <rungenesamuel@gmail.com>
2021-03-15 19:23:43 +0300

    Set up the shapes

--- app/src/main/java/com/example/android/clippingexample/ClippedView.kt
+++ app/src/main/java/com/example/android/clippingexample/ClippedView.kt
@@ -1,6 +1,8 @@
 package com.example.android.clippingexample
 
 import android.content.Context
+import android.graphics.Paint
+import android.graphics.Path
 import android.util.AttributeSet
 import android.view.View
 
@@ -11,5 +13,33 @@ class ClippedView @JvmOverloads constructor(
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
+    private val paint = Paint().apply {
+        // Smooth out edges of what is drawn without affecting shape.
+        isAntiAlias = true
+        strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+        textSize = resources.getDimension(R.dimen.textSize)
+    }
+
+    //create and initialize a path variable of a Path to store locally the path of what has been drawn.
+    private val path = Path()
+
+    //add variables for dimensions for a clipping rectangle around the whole set of shapes.
+    private val clipRectRight = resources.getDimension(R.dimen.clipRectRight)
+    private val clipRectBottom = resources.getDimension(R.dimen.clipRectBottom)
+    private val clipRectTop = resources.getDimension(R.dimen.clipRectTop)
+    private val clipRectLeft = resources.getDimension(R.dimen.clipRectLeft)
+
+    //Add variables for the inset of a rectangle and the offset of a small rectangle.
+    private val rectInset = resources.getDimension(R.dimen.rectInset)
+    private val smallRectOffset = resources.getDimension(R.dimen.smallRectOffset)
+
+    //Add a variable for the radius of a circle. This is the circle that is drawn inside the rectangle.
+    private val circleRadius = resources.getDimension(R.dimen.circleRadius)
+
+   // Add an offset and a text size for text that is drawn inside the rectangle.
+   private val textOffset = resources.getDimension(R.dimen.textOffset)
+    private val textSize = resources.getDimension(R.dimen.textSize)
+
 
 }
