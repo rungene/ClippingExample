1c586dc45e75cbcd7d0dc758dc1bbca9c8287586
rungene <rungenesamuel@gmail.com>
2021-03-15 19:11:58 +0300

    Add Dimensions and String Resources

--- app/src/main/java/com/example/android/clippingexample/ClippedView.kt
+++ app/src/main/java/com/example/android/clippingexample/ClippedView.kt
@@ -4,9 +4,12 @@ import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 
+//The @JvmOverloads annotation instructs the Kotlin compiler to generate overloads for this
+// function that substitute default parameter values.
 class ClippedView @JvmOverloads constructor(
     context: Context,
     attrs: AttributeSet? = null,
     defStyleAttr: Int = 0
 ) : View(context, attrs, defStyleAttr) {
+
 }
